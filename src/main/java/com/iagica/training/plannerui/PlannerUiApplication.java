27cd67cccb581a6ad5612ccbd6b152951c37ba9e
Roberto Angelucci <roberto.angelucci@gmail.com>
2023-04-27 17:44:06 +0200

    Aggiunto scheletro del progetto ma ancora non funzionante

---
+++ src/main/java/com/iagica/training/plannerui/PlannerUiApplication.java
@@ -0,0 +1,13 @@
+package com.iagica.training.plannerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerUiApplication.class, args);
+	}
+
+}
