21160fc58a8dbab984ec2fa2eb9f1678d49f1ddf
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2023-04-28 17:18:27 +0200

    Modifiche front

--- src/main/webapp/planner-ng/src/app/app.component.spec.ts
+++ src/main/webapp/planner-ng/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'planner-ui'`, () => {
+  it(`should have as title 'planner-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('planner-ui');
+    expect(app.title).toEqual('planner-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('planner-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('planner-ng app is running!');
   });
 });
