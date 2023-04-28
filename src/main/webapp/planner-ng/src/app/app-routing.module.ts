cb0b725e844c67e58f5b7c00e31f06d7a8794026
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2023-04-28 15:35:54 +0200

    Configurazione Login e Registrazione User

--- src/main/webapp/planner-ng/src/app/app-routing.module.ts
+++ src/main/webapp/planner-ng/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
+import { AuthGuardService } from './guard/auth.guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/webapp/planner-ng/src/app/app.module.ts
+++ src/main/webapp/planner-ng/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './pages/home/home/home.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './interceptor/token.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
