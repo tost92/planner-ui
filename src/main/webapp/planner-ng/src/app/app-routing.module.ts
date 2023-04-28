00352d6234a2fc05a2af1e57d191e2d66074f809
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2023-04-28 18:05:41 +0200

    Allineamento Codice Login e Registrazione

--- src/main/webapp/planner-ng/src/app/app-routing.module.ts
+++ src/main/webapp/planner-ng/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
+import { AuthGuardService } from './guard/auth.guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }///ciaoooo
+export class AppRoutingModule { }
